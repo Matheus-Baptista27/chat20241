daaf45f41024b8d4ce98b7a9b29399eed9392453
Matheus_Baptista <matheus.baptista07@hotmail.com>
2024-03-20 22:00:01 -0300

    token listar salas

--- src/util/token.js
+++ src/util/token.js
@@ -13,19 +13,15 @@ exports.entrar=async(nick)=>{
     const jwt = require(`jsonwebtoken`);
 
     const checktoken = async (token, id, key) => jwt.verify(token, key, (err, decoded) => {
-        
-    function verifyJWT(req, res, next){
-    const token = req.headers['authorization'];
-    if (!token) return res.status(401).json({ auth: false, message: 'No token provided.' });
-    
-    jwt.verify(token, process.env.SECRET, function(err, decoded) {
-      if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
-      
-      // se tudo estiver ok, salva no request para uso posterior
-      req.userId = decoded.id;
-      next();
-    });
-}
+        try {
+            let decoded = jwt.verify(token, key);
+            if(decoded){
+                if(decoded.id==id) return true;
+            }
+            return false;
+        }catch(e){
+            return false
+        }
     })
     const setToken = async (id, key) => {
         console.log(id);
